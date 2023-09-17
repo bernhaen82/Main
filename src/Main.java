d5e5447773b5a65db2cb76c5e2f744d427dfdf96
Ethan B <bernhaen@mail.uc.edu>
2023-09-17 01:15:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args)
+    {
+    // intOperandA, intOperandB, insum, intProduct, intDifference, intQuotient, intModulo
+
+        int intOperandA = 4;
+        int intOperandB = 7;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println(" The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        intProduct = intOperandA * intOperandB;
+        System.out.println(" The Product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        double doubleOperandA = 3.14;
+        double doubleOperandB = 3.50;
+        double doubleSum = 0;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println(" The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        // do the rest of the ruble examples
+
+        
+
+
+
+
+
+
+
+
+    }
+}
